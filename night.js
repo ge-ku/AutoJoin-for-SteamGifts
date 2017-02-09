749910cbb953a4b289192d4f95b651cd6070daed
ge-ku <geku94@gmail.com>
2017-02-09 21:36:25 +0200

    night theme to use correct setting value

--- night.js
+++ night.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
-	chrome.storage.sync.get("nightTheme", function(data) {
-		if (typeof data['nightTheme'] == 'undefined'){
+	chrome.storage.sync.get("NightTheme", function(data) {
+		if (typeof data.NightTheme == 'undefined'){
 			chrome.storage.sync.set({'nightTheme': 'false'});
 		}else{
-			if (data['nightTheme'] == 'true'){
+			if (data.NightTheme == true){
 				var path = chrome.extension.getURL('/night.css');
 				$('head').append($('<link>')
 					.attr("rel","stylesheet")
