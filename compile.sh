c82a7bf5054ed216ed61ec95fc76246d6bac046d
Gena <geku94@gmail.com>
2017-09-22 10:05:33 +0300

    Build Firefox version with specific manifest.json

--- compile.sh
+++ compile.sh
@@ -1,20 +1,20 @@
 # !/bin/bash
 
-# This script will minify javascript using Closure Compiler and pack everything into AutoJoin_{VERSION}.zip ready for uploading to the Chrome Store.
+# This script will minify javascript using Closure Compiler and pack everything into AutoJoin_[BROWSER]_{VERSION}.zip ready for publishing.
 # This way we can use unpacked extension while developing without changing any filename or manifest.json or manually replacing files.
 # Use "Bash on Ubuntu on Windows" if you're on Windows 10 (it's awesome!) or cygwin to run on Windows.
 
 clear
 
 VERSION=$(grep '"version":' manifest.json | sed 's/^.*: //;s/"//g' | tr -d ',\r\n');
-echo "AutoJoin version in manifest.json: $VERSION. This script will minify javascript using Closure Compiler and pack everything into AutoJoin_${VERSION}.zip";
+echo -e "AutoJoin version in manifest.json: $VERSION.\nThis script will minify javascript using Closure Compiler and pack everything into AutoJoin_[BROWSER]_${VERSION}.zip";
 
-echo "Creating temp folder that will hold minified scripts, it'll be deleted in the end...";
+echo "Creating temp folder that will hold minified scripts and manifest.json, it'll be deleted in the end...";
 mkdir temp;
 
-echo "Creating AutoJoin_${VERSION}.zip with icons, mp3 file, manifest and jquery...";
+echo "Creating AutoJoin_[BROWSER]_${VERSION}.zip with icons, mp3 file, manifest and jquery...";
 
-zip "AutoJoin_${VERSION}.zip" autologo.png autologo16.png autologo48.png autologosteam.png audio.mp3 jquery.min.js manifest.json chromeOptions.css general.css night.css settings.html ;
+zip "AutoJoin_Chrome_${VERSION}.zip" autologo.png autologo16.png autologo48.png autologosteam.png audio.mp3 jquery.min.js chromeOptions.css general.css night.css settings.html ;
 
 echo "Minifying js files (besides jquery) using Closure Compiler...";
 for jsfile in *.js; do
@@ -32,9 +32,15 @@ for jsfile in *.js; do
 done
 
 echo "Adding minified js files into AutoJoin_${VERSION}.zip...";
+zip -j "AutoJoin_Chrome_${VERSION}.zip" temp/*.js;
 
-zip -j "AutoJoin_${VERSION}.zip" temp/*.js;
+echo "Adding extra lines into manifest file needed for Firefox...";
+cp "AutoJoin_Chrome_${VERSION}.zip" "AutoJoin_Firefox_${VERSION}.zip";
+zip "AutoJoin_Chrome_${VERSION}.zip" manifest.json;
+cp manifest.json temp/manifest.json;
+firefox_specific_bits='\   \"applications\": {\n\      \"gecko\": {\n\         \"id\": \"banchecker@kuzmenko.io\",\n\         \"strict_min_version\": \"48.0\"\n\      }\n\   },';
+sed -i "/\"manifest_version\": 2/i $firefox_specific_bits" temp/manifest.json;
+zip -j "AutoJoin_Firefox_${VERSION}.zip" temp/manifest.json;
 
 rm -rf temp;
-
 echo "Done!";
