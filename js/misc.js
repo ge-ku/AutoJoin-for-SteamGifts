b717f4f6d4be24a6fb26938b4733da30b9b2eaf9
Геннадий <geku94@gmail.com>
2020-07-25 12:33:50 +0300

    Fixed pinning header and footer functionality

--- js/misc.js
+++ js/misc.js
@@ -9,71 +9,74 @@ chrome.storage.sync.get({ NightTheme: false }, (data) => {
   }
 });
 
-/* Pin navigation bar */
-const navbar = document.querySelector('.nav__left-container');
-let navbarPinned = false;
-const navbarPinIcon = document.createElement('i');
-navbarPinIcon.className = 'fa fa-thumb-tack';
-const navbarPin = document.createElement('div');
-navbarPin.className = 'nav__button-container nav__button-container--notification nav__button-container--inactive';
-navbarPin.id = 'navbarPin';
-navbarPin.title = 'Pin navigation bar';
-navbarPin.appendChild(navbarPinIcon);
-navbar.prepend(navbarPin);
-navbarPin.addEventListener('click', () => {
-  chrome.storage.local.set({ pinnedNavbar: !navbarPinned }, evalPinnedNavbar);
-});
-const bufferEl = document.createElement('div');
-bufferEl.id = 'bufferEl';
-document.querySelector('body').prepend(bufferEl);
-
-function evalPinnedNavbar() {
-  chrome.storage.local.get({ pinnedNavbar: true }, (data) => {
-    if (data.pinnedNavbar) {
-      navbarPinned = true;
-      navbarPin.classList.remove('nav__button-container--inactive');
-      navbarPin.classList.add('nav__button-container--active');
-      bufferEl.className = 'pinned';
-      document.querySelector('header').classList.add('pinned');
-    } else {
-      navbarPinned = false;
-      navbarPin.classList.remove('nav__button-container--active');
-      navbarPin.classList.add('nav__button-container--inactive');
-      bufferEl.className = '';
-      document.querySelector('header').classList.remove('pinned');
-    }
+document.addEventListener('DOMContentLoaded', () => {
+  /* Pin navigation bar */
+  const navbar = document.querySelector('.nav__left-container');
+  let navbarPinned = false;
+  const navbarPinIcon = document.createElement('i');
+  navbarPinIcon.className = 'fa fa-thumb-tack';
+  const navbarPin = document.createElement('div');
+  navbarPin.className =
+    'nav__button-container nav__button-container--notification nav__button-container--inactive';
+  navbarPin.id = 'navbarPin';
+  navbarPin.title = 'Pin navigation bar';
+  navbarPin.appendChild(navbarPinIcon);
+  navbar.prepend(navbarPin);
+  navbarPin.addEventListener('click', () => {
+    chrome.storage.local.set({ pinnedNavbar: !navbarPinned }, evalPinnedNavbar);
   });
-}
-evalPinnedNavbar();
+  const bufferEl = document.createElement('div');
+  bufferEl.id = 'bufferEl';
+  document.querySelector('body').prepend(bufferEl);
 
-/* Pin footer */
-const ajFooter = document.createElement('div');
-ajFooter.id = 'ajFooter';
-const ajFooterArrowWrap = document.createElement('div');
-ajFooterArrowWrap.id = 'ajFooterArrowWrap';
-const ajFooterArrow = document.createElement('div');
-ajFooterArrow.id = 'ajFooterArrow';
-ajFooterArrow.textContent = '▲';
-ajFooterArrowWrap.appendChild(ajFooterArrow);
-ajFooter.appendChild(ajFooterArrowWrap);
+  const evalPinnedNavbar = () => {
+    chrome.storage.local.get({ pinnedNavbar: true }, (data) => {
+      if (data.pinnedNavbar) {
+        navbarPinned = true;
+        navbarPin.classList.remove('nav__button-container--inactive');
+        navbarPin.classList.add('nav__button-container--active');
+        bufferEl.className = 'pinned';
+        document.querySelector('header').classList.add('pinned');
+      } else {
+        navbarPinned = false;
+        navbarPin.classList.remove('nav__button-container--active');
+        navbarPin.classList.add('nav__button-container--inactive');
+        bufferEl.className = '';
+        document.querySelector('header').classList.remove('pinned');
+      }
+    });
+  };
+  evalPinnedNavbar();
 
-const sgFooter = document.querySelector('.footer__outer-wrap');
-sgFooter.parentElement.insertBefore(ajFooter, sgFooter);
-ajFooter.appendChild(sgFooter);
+  /* Pin footer */
+  const ajFooter = document.createElement('div');
+  ajFooter.id = 'ajFooter';
+  const ajFooterArrowWrap = document.createElement('div');
+  ajFooterArrowWrap.id = 'ajFooterArrowWrap';
+  const ajFooterArrow = document.createElement('div');
+  ajFooterArrow.id = 'ajFooterArrow';
+  ajFooterArrow.textContent = '▲';
+  ajFooterArrowWrap.appendChild(ajFooterArrow);
+  ajFooter.appendChild(ajFooterArrowWrap);
 
-let footerPinned = false;
-ajFooterArrowWrap.addEventListener('click', () => {
-  chrome.storage.local.set({ pinnedFooter: !footerPinned }, evalPinnedFooter);
-});
+  const sgFooter = document.querySelector('.footer__outer-wrap');
+  sgFooter.parentElement.insertBefore(ajFooter, sgFooter);
+  ajFooter.appendChild(sgFooter);
 
-function evalPinnedFooter() {
-  chrome.storage.local.get({ pinnedFooter: true }, (data) => {
-    footerPinned = data.pinnedFooter;
-    if (footerPinned) {
-      ajFooter.className = 'pinned';
-    } else {
-      ajFooter.className = '';
-    }
+  let footerPinned = false;
+  ajFooterArrowWrap.addEventListener('click', () => {
+    chrome.storage.local.set({ pinnedFooter: !footerPinned }, evalPinnedFooter);
   });
-}
-evalPinnedFooter();
+
+  const evalPinnedFooter = () => {
+    chrome.storage.local.get({ pinnedFooter: true }, (data) => {
+      footerPinned = data.pinnedFooter;
+      if (footerPinned) {
+        ajFooter.className = 'pinned';
+      } else {
+        ajFooter.className = '';
+      }
+    });
+  };
+  evalPinnedFooter();
+});
